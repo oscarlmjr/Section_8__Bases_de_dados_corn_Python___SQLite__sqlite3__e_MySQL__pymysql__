dccb0f84ac94f839d0a47394b7d0e05a5ed34ebe
oscarlmjr <oscarlmjr@gmail.com>
2026-02-27 10:28:46 -0300

    secao_8 aula_393

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# CUIDADO: fazendo delete sem where
+cursor.execute(
+	f'DELETE FROM {TABLE_NAME}'
+)
+connection.execute(
+	f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+# Cria a tabela
 cursor.execute(
 	f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
 	'('
@@ -17,6 +27,16 @@ cursor.execute(
 	'weight REAL'
 	')'
 )
+connection.commit()
+
+# Registrar valores nas colunas das tabela
+# CUIDADO: sql injection
+cursor.execute(
+	f'INSERT INTO {TABLE_NAME} '
+	'(id, name, weight) '
+	'VALUES '
+	'(NULL, "Helena", 4), (NULL, "Eduardo", 10)'
+	)
 connection.commit()
 
 cursor.close()
